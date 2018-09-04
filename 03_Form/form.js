6599b4d3536b6fff306bf56c3117ef70f41d2202
indian705 <indian705@hotmail.com>
2018-09-04 12:45:23 +0200

    03_Form

--- 03_Form/form.js
+++ 03_Form/form.js
@@ -1,10 +1,19 @@
 export class Form {
     constructor() {
         this.domForm = document.querySelector('form')
-        this.domUser = document.querySelector('#user')
         this.data = {
-            user : '',
-            passw : ''
+            opciones : {
+                conditions: '',
+                turn: '',
+                curse: ''
+            },
+            user : { 
+                name: '', 
+                f_name: ''},
+            email: '',
+            passw : '',
+            coments : '',
+            aficiones : []
         }
         this.domForm.addEventListener('submit',
             this.enviar.bind(this)
@@ -13,9 +22,68 @@ export class Form {
 
     enviar(oEv) {
         oEv.preventDefault()
-        // Dos formas de recoger los datos
-        this.data.user = this.domUser.value
+        this.data.user.name = document.querySelector('#nombre').value
+        //this.data.user.name = this.domForm.elements.nombre.value
+        this.data.user.f_name = document.querySelector('#apellido').value
+        this.data.email = document.querySelector('#correo').value
         this.data.passw = document.querySelector('#passw').value
+        this.data.coments = document.querySelector('#comentarios').value
+        this.data.opciones.conditions = document.querySelector('#condiciones').checked
+        this.data.opciones.turn = this.verTurno()
+        this.data.opciones.curse = this.verCurso()
+        this.data.aficiones = this.setAficiones()
         console.log(this.data)
+        //console.dir(this.domForm)
+        console.dir()
     }
+
+    verTurno() {
+        // let aTurnos = document.getElementsByName('turno')
+        let aTurnos = document.querySelectorAll('[name="turno"]')
+        for (let i = 0; i < aTurnos.length; i++) {
+            const item = aTurnos[i];
+            if (item.checked) {
+                return item.value
+            }
+        }
+        /* let r =''
+        let aDatos = []
+        aTurnos.forEach(
+            item => aDatos.push(item)
+        )
+        aDatos.some(    
+            item => {
+                if (item.checked) {
+                    r = item.value
+                    return true
+                }
+            })
+        return r */
+    }
+
+    verCurso() {
+        const curso = document.querySelector('#curso')
+        console.dir(curso.selectedIndex)
+        console.dir(curso[curso.selectedIndex])
+        const oDato = {
+            value :  curso[curso.selectedIndex].value,
+            text :  curso[curso.selectedIndex].textContent
+        }
+        return oDato
+    }
+
+    setAficiones(){
+        let aDatos = []
+        let aAficiones = document.querySelectorAll('.aficiones')
+        aAficiones.forEach( item => {
+            if(item.checked){
+                aDatos.push(item.id)
+            }
+        })
+        return aDatos
+    }
+    
+
+    
 }
+
