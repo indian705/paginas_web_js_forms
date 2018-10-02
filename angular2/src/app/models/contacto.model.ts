4ad80286de6f2874e08634002f5f95b42a31dd2d
indian705 <indian705@hotmail.com>
2018-10-02 13:59:17 +0200

    angular2

---
+++ angular2/src/app/models/contacto.model.ts
@@ -0,0 +1,10 @@
+export interface ContactoIf {
+    fullname: {
+        nombre: string,
+        apellido: string
+    }
+    telefono: number
+    email: string
+    avatar: string
+
+}
--- angular2/src/app/tareas/tarea-plus/tarea-plus.component.ts
+++ angular2/src/app/tareas/tarea-plus/tarea-plus.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TareaModel, TareaIf } from '../../models/tarea.model';
+
 
 @Component({
   selector: 'cas-tarea-plus',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tarea-plus.component.css']
 })
 export class TareaPlusComponent implements OnInit {
+  //newTarea: TareaModel
+  newTarea: TareaIf
+  aTareas: Array<TareaIf>
 
   constructor() { }
 
   ngOnInit() {
+    //this.newTarea = new TareaModel('', false)
+    this.newTarea = {nombre:'', isCompleted: false}
+    this.aTareas = []
+  }
+
+  addTarea() {
+    if(!this.newTarea.nombre) {return}
+    console.log(this.newTarea)
+    //this.aTareas.push(JSON.parse(JSON.stringify(this.newTarea)))
+    this.aTareas.push(Object.assign({}, this.newTarea))
+    console.log(this.aTareas)
+    this.newTarea.nombre = ''
+  }
+
+  borrarTarea(i){
+    this.aTareas.splice(i,1)
   }
 
 }
